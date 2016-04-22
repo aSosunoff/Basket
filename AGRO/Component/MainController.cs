883b93531dc3ac241ace0580efbfbc7dbde3cbc4
aSosunoff <al.sosunoff@gmail.com>
2016-04-22 15:31:19 +0300

    Added MainController

---
+++ AGRO/Component/MainController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.Engine.Service;
+
+namespace AGRO.Component
+{
+    public class MainController : Controller
+    {
+        public IServiceLayer _serviceLayer { get; set; }
+        public StartViewBag StartViewBag { get; set; }
+
+        public MainController(IServiceLayer serviceLayer)
+        {
+            _serviceLayer = ServiceLayer.Instance(serviceLayer);
+
+            StartViewBag = new StartViewBag(_serviceLayer);
+
+            ViewBag.CountElementToBasket = StartViewBag.CountElementToBasket;
+            ViewBag.CountElementToContract = StartViewBag.CountElementToContract;
+            ViewBag.WrapModels = StartViewBag.WrapModels;
+
+
+        }
+    }
+}
